6419b8273fec12b0327d836c3fad3f48f192dadd
YiYeHuang <tonyhuang0446@gmail.com>
2018-11-05 11:47:33 -0800

    orginal heart beat monitor implementation checkin

---
+++ src/main/java/oldImpl/DummyService/src/dummyserver/Service.java
@@ -0,0 +1,32 @@
+package oldImpl.DummyService.src.dummyserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Service implements Runnable
+{
+    private int port;
+    public Service(int port)
+    {
+        this.port = port;
+    }
+
+    public void run()
+    {
+        ServerSocket testSocket;
+        try
+        {
+            testSocket = new ServerSocket(port);
+            System.out.println("Service start at: " + port);
+            while (true)
+            {
+                Socket connectionSocket = testSocket.accept();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/oldImpl/DummyService/src/launch/LaunchDummyService.java
@@ -0,0 +1,21 @@
+package oldImpl.DummyService.src.launch;
+
+import oldImpl.DummyService.src.dummyserver.Service;
+
+public class LaunchDummyService
+{
+    public static void main(String argv[]) throws Exception
+    {
+        Service s1 = new Service(8989);
+        Service s2 = new Service(6789);
+        Service s3 = new Service(9999);
+        
+        Thread t1 = new Thread(s1);
+        Thread t2 = new Thread(s2);
+        Thread t3 = new Thread(s3);
+        
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
